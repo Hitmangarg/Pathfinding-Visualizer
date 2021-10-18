5415a9ae2cbca6a5b5f9ee7925cbce70ee510c5f
Hitmangarg <hiteshgarg4000@gmail.com>
2021-10-18 21:49:34 +0530

    First commit

---
+++ src/components/Path/Path.js
@@ -0,0 +1,11 @@
+import BreadthFirstSearch from "./BreadthFirstSearch";
+import DepthFirstSearch from "./DepthFirstSearch";
+import Dijkstra from "./Dijkstra";
+
+// wrapper json for all functionality
+export default {
+  BreadthFirstSearch: BreadthFirstSearch,
+  DepthFirstSearch: DepthFirstSearch,
+  Dijkstra: Dijkstra,
+  none: function() {}
+};
